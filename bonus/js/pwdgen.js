67b117ddc7b2107ac041b15228e19fff46f8a0d9
marcocanopoli <canopoli.marco@gmail.com>
2021-04-26 18:03:04 +0200

    debug instructions removed

--- bonus/js/pwdgen.js
+++ bonus/js/pwdgen.js
@@ -1,10 +1,16 @@
 function generatePassword() {
 
-    var firstName = document.getElementById("first_name").value;
-    var lastName = document.getElementById("last_name").value;
-    var favColor = document.getElementById("color").value;
-    var myPwd = firstName + lastName + favColor + "21";
+  var firstName = document.getElementById("first_name").value;
+  var lastName = document.getElementById("last_name").value;
+  var favColor = document.getElementById("color").value;
+  var myPwd = firstName + lastName + favColor + "21";
+  var blank = "";
 
-    document.getElementById("password").innerHTML = myPwd;
-    console.log(myPwd);    
-  }
+  document.getElementById("password").innerHTML = myPwd;
+
+  //reset inputs
+  document.getElementById("first_name").value = blank;
+  document.getElementById("last_name").value = blank;
+  document.getElementById("color").value = blank;
+  
+}
