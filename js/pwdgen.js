4c43365d60847ce58fae7f7d73b6d25426cc9fae
marcocanopoli <canopoli.marco@gmail.com>
2021-04-26 15:46:30 +0200

    Pwd var added

--- js/pwdgen.js
+++ js/pwdgen.js
@@ -3,5 +3,8 @@ var firstName = prompt("Inserisci il tuo nome");
 var lastName = prompt("Inserisci il tuo cognome");
 var favColor = prompt("Qual e' il tuo colore preferito?");
 
+//generate pwd
+var myPwd = firstName + lastName + favColor + "21";
+
 //display generated pwd
-document.getElementById("my_pwd").innerHTML = firstName + lastName + favColor + "21";
+document.getElementById("password").innerHTML = myPwd;
