e6097a4a53308fed66e17d64924745f646b64175
marcocanopoli <canopoli.marco@gmail.com>
2021-04-26 18:05:15 +0200

    inputs reset added

--- bonus/js/pwdgen.js
+++ bonus/js/pwdgen.js
@@ -4,13 +4,12 @@ function generatePassword() {
   var lastName = document.getElementById("last_name").value;
   var favColor = document.getElementById("color").value;
   var myPwd = firstName + lastName + favColor + "21";
-  var blank = "";
 
   document.getElementById("password").innerHTML = myPwd;
 
   //reset inputs
-  document.getElementById("first_name").value = blank;
-  document.getElementById("last_name").value = blank;
-  document.getElementById("color").value = blank;
+  document.getElementById("first_name").value = "";
+  document.getElementById("last_name").value = "";
+  document.getElementById("color").value = "";
   
 }
